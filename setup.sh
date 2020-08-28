54b104ea280032ceeeaa78aecac4b076feee1935
ChrisRodgers-code <60756647+ChrisRodgers-code@users.noreply.github.com>
2020-08-28 07:25:25 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+mkdir $HOME/bin $HOME/PasswordManager
+
+echo 'export PATH="$HOME/bin:$PATH"' >> $HOME/.profile
+
+mv passman $HOME/bin/; mv passman $HOME/bin/
+mv pass_manager.py $HOME/PasswordManager/
